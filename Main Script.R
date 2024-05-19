c24521a78af3480e453537459ef26694d343de70
Ben Harley <harleyb101010@gmail.com>
2024-05-19 18:28:39 +0100

    details editted

--- Main Script.R
+++ Main Script.R
@@ -1,9 +1,9 @@
 ###################################################################################
-# Welcome to the SAPD Weapon License Script and Salary Outgoings Calculator       #
+# Welcome to the SAPD Weapon License Script                                       #
 # This script is designed to do the following:                                    #
 # *Allocate a player's column to a faction based on SAPD/SASF/NOOSE staff rosters #
-# *Calculate how much in total SAPD is spending over a 2 week period in salaries  #
-# *Create a ratio to see how this varies across total players with above 0 hours  #
+# *No Badge Number Report                                                         #
+# *Activity Report                                                                #
 ##########
 #########################################################################
 
